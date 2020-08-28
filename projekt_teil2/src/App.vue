6a4f7764b0b1ee35bf7bb7104f6ae780ccac415d
Bernhard <b.korn@boservice.de>
2020-08-28 19:39:11 +0200

    Teil 2 angefangen

---
+++ projekt_teil2/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    
+  {{Name}},
+  {{Alter}},
+  {{Geschlecht}}
+  <br>
+  {{'${Name}'}}
+  <br>
+  {{Alter*2}}
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data: () =>
+  ({
+    Name: 'Bernhard',
+    Alter: 52,
+    Geschlecht: 'männlich'
+
+  })
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
